66bc728b38e35b09859c7dd3fe35e9ec7fc43566
André Favotto <42416997+AndreFavotto@users.noreply.github.com>
2018-12-04 15:30:43 -0200

    Update p6.c

--- p6.c
+++ p6.c
@@ -10,26 +10,26 @@ typedef
 ponto;
 
 ponto * gerar_pontos(int N) {
-    ponto * coordenadas = (ponto *) calloc(N,sizeof(ponto));
+    ponto * coord = (ponto *) calloc(N,sizeof(ponto));
     int i;
     for(i = 0; i < N; i++){
-        coordenadas[i].x = cos(i*2.0*M_PI/(N-1));
-        coordenadas[i].y = sin(i*2.0*M_PI/(N-1));
+        coord[i].x = cos(i*2.0*M_PI/(N-1));
+        coord[i].y = sin(i*2.0*M_PI/(N-1));
     }
-    return coordenadas;
+    return coord;
 }
 
-void mostrar_pontos(ponto * p_ini, ponto * p_fim) {
-    if(p_ini < p_fim) {
-        printf("x: %.3lf, y: %.3lf\n", p_ini->x, p_ini->y);
-        mostrar_pontos(p_ini+1, p_fim);
+void mostrar_pontos(ponto * pbeg, ponto * pend) {
+    if(pbeg < pend) {
+        printf("x: %.3lf, y: %.3lf\n", pbeg->x, pend->y);
+        mostrar_pontos(pbeg+1, pend);
     }
 }
 
 int main(){
     unsigned int N;
     ponto *coordenadas;
-    printf("Digite o número de pontos: ");
+    printf("Digite o nÃºmero de pontos: ");
     scanf("%u", &N); getchar();
     coordenadas = gerar_pontos(N);
     mostrar_pontos(coordenadas, coordenadas + N);
